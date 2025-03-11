0473bb9510e3f6036300b7362fbfe096c3105ae0
KRISHNA-LEGEND-BOY <rohitprajapati052005@gmail.com>
2025-03-11 18:42:00 +0530

    Update admin_det.js

--- Priyansh/commands/admin_det.js
+++ Priyansh/commands/admin_det.js
@@ -14,9 +14,9 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
   if (event.body.indexOf("ADMIN")==0 || event.body.indexOf("Admin")==0 || event.body.indexOf("/Admin")==0 || event.body.indexOf("#admin")==0) {
     var msg = {
-        body: "🫅 𝐎𝐖𝐍𝐄𝐑 𝐏𝐑𝐎𝐅𝐈𝐋𝐄 🫅",
+        body: "🫅 𝐎𝐖𝐍𝐄𝐑 𝐏𝐑𝐎𝐅𝐈𝐋𝐄 🫅\n✧═════════•❁❀❁•═════════✧\n🌸🐬 𝐎𝐖𝐍𝐄𝐑 :- 𝐊𝐑𝐈𝐒𝐇𝐍𝐀 😘🥰",
         attachment: 
-fs.createReadStream(__dirname + `/noprefix/Screenshot_2024-12-06-10-28-48-49_8110815a90a63ac3d3d945cf32a1cf14.jpg`)
+fs.createReadStream(__dirname + `/noprefix/Messenger_creation_610769315206850.jpeg`)
       }
       api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🫅", event.messageID, (err) => {}, true)
