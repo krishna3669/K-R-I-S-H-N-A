378862e01360c825dec7b9d4f8e26c7426a29be1
KRISHNA-LEGEND-BOY <rohitprajapati052005@gmail.com>
2025-03-12 00:57:20 +0530

    Update pair1.js

--- Priyansh/commands/pair1.js
+++ Priyansh/commands/pair1.js
@@ -1,81 +1,48 @@
 module.exports.config = {
-    name: "pair1",
-    version: "1.0.1",
-    hasPermssion: 0,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
-    description: "Pair with people in the group",
-    commandCategory: "tình yêu",
-    cooldowns: 5,
-    dependencies: {
-        "axios": "",
-        "fs-extra": ""
-    }
-}
-module.exports.onLoad = async() => {
-    const { resolve } = global.nodemodule["path"];
-    const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
-    const { downloadFile } = global.utils;
-    const dirMaterial = __dirname + `/cache/canvas/`;
-    const path = resolve(__dirname, 'cache/canvas', 'pairing.jpg');
-    if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
-    if (!existsSync(path)) await downloadFile("https://i.pinimg.com/736x/15/fa/9d/15fa9d71cdd07486bb6f728dae2fb264.jpg", path);
-}
+  name: "pair1",
+  version: "1.0.0", 
+  hasPermssion: 0,
+  credits: "KRISHNA",
+  description: "THIS BOT IS MR KRISHNA",
+  commandCategory: "COUPLE LOVE PAIRING", 
+  usages: "pair1", 
+  cooldowns: 15
+};
+module.exports.run = async function({ api, event,Threads, Users }) {
+        const axios = global.nodemodule["axios"];
+        const fs = global.nodemodule["fs-extra"];
 
-async function makeImage({ one, two }) {
-    const fs = global.nodemodule["fs-extra"];
-    const path = global.nodemodule["path"];
-    const axios = global.nodemodule["axios"]; 
-    const jimp = global.nodemodule["jimp"];
-    const __root = path.resolve(__dirname, "cache", "canvas");
-
-    let pairing_img = await jimp.read(__root + "/pairing.jpg");
-    let pathImg = __root + `/pairing_${one}_${two}.png`;
-    let avatarOne = __root + `/avt_${one}.png`;
-    let avatarTwo = __root + `/avt_${two}.png`;
-    
-    let getAvatarOne = (await axios.get(`https://graph.facebook.com/${one}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
-    fs.writeFileSync(avatarOne, Buffer.from(getAvatarOne, 'utf-8'));
-    
-    let getAvatarTwo = (await axios.get(`https://graph.facebook.com/${two}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
-    fs.writeFileSync(avatarTwo, Buffer.from(getAvatarTwo, 'utf-8'));
-    
-    let circleOne = await jimp.read(await circle(avatarOne));
-    let circleTwo = await jimp.read(await circle(avatarTwo));
-    pairing_img.composite(circleOne.resize(85, 85), 355, 100).composite(circleTwo.resize(75, 75), 250, 140);
-    
-    let raw = await pairing_img.getBufferAsync("image/png");
-    
-    fs.writeFileSync(pathImg, raw);
-    fs.unlinkSync(avatarOne);
-    fs.unlinkSync(avatarTwo);
-    
-    return pathImg;
-}
-async function circle(image) {
-    const jimp = require("jimp");
-    image = await jimp.read(image);
-    image.circle();
-    return await image.getBufferAsync("image/png");
-}
-module.exports.run = async function({ api, event, args, Users, Threads, Currencies }) {
-  const axios = require("axios");
-    const fs = require("fs-extra");
-    const { threadID, messageID, senderID } = event;
-    var tl = ['21%', '67%', '19%', '37%', '17%', '96%', '52%', '62%', '76%', '83%', '100%', '99%', "0%", "48%"];
-        var tle = tl[Math.floor(Math.random() * tl.length)];
-        let dataa = await api.getUserInfo(event.senderID);
-        let namee = await dataa[event.senderID].name
-        let loz = await api.getThreadInfo(event.threadID);
-        var emoji = loz.participantIDs;
-        var id = emoji[Math.floor(Math.random() * emoji.length)];
-        let data = await api.getUserInfo(id);
-        let name = await data[id].name
+        var { participantIDs } =(await Threads.getData(event.threadID)).threadInfo;
+        var tle = Math.floor(Math.random() * 101);
+        var namee = (await Users.getData(event.senderID)).name
+        const botID = api.getCurrentUserID();
+        const listUserID = event.participantIDs.filter(ID => ID != botID && ID != event.senderID);
+        var id = listUserID[Math.floor(Math.random() * listUserID.length)];
+        var name = (await Users.getData(id)).name
         var arraytag = [];
                 arraytag.push({id: event.senderID, tag: namee});
                 arraytag.push({id: id, tag: name});
-        
-        var sex = await data[id].gender;
-        var gender = sex == 2 ? "Male🧑" : sex == 1 ? "Female👩‍🦰" : "Tran Duc Bo";
-var one = senderID, two = id;
-    return makeImage({ one, two }).then(path => api.sendMessage({ body:`Congratulations ${namee} was paired with ${name}\nPair odds are: ${tle}`, mentions: arraytag, attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
-  }
+
+  
+        let Avatar = (await axios.get( `https://graph.facebook.com/${event.senderID}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: "arraybuffer" } )).data; 
+            fs.writeFileSync( __dirname + "/cache/avt.png", Buffer.from(Avatar, "utf-8") );
+
+        let gifLove = (await axios.get( `https://i.imgur.com/sWKpIKL.gif`, { responseType: "arraybuffer" } )).data; 
+            fs.writeFileSync( __dirname + "/cache/giflove.png", Buffer.from(gifLove, "utf-8") );
+
+        let Avatar2 = (await axios.get( `https://graph.facebook.com/${id}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: "arraybuffer" } )).data;
+            fs.writeFileSync( __dirname + "/cache/avt2.png", Buffer.from(Avatar2, "utf-8") );
+
+        var imglove = [];
+              
+              imglove.push(fs.createReadStream(__dirname + "/cache/avt.png"));
+              imglove.push(fs.createReadStream(__dirname + "/cache/giflove.png"));
+              imglove.push(fs.createReadStream(__dirname + "/cache/avt2.png"));
+
+        var msg = {body: `🌸===『*★𝗖𝗿𝗲𝗱𝗶𝘁:- 𝐊𝐑𝐈𝐒𝐇𝐍𝐀_____________________________________________________________________‎‎[•||•●•||•┼┼──🌸,,💟..ƙɧ𝘂ɗ𝗔  ƙi͠ 𝗴𝘂r᩶𝐬𝗔𝗧 ɱɘ ɘƙ p᩶𝗔ɭ 𝗔𝗔ɣ𝗔 ɧ❍𝗴𝗔 ʝ𝗔ß 𝘂𝐬ɳɘ 𝗧𝘂ʝɧɘ ʝɘ𝐬𝗔 p᩶ɣ𝗔r᩶ i͠ɳ𝐬𝗔𝗔ɳ ß𝗔ɳ𝗔ɣ𝗔 ɧ❍𝗴𝗔...💗 🥰•||•🐬•||•]]
+🌹✦━━━━━🌹━━🌹━━━━━✦🌹
+\n[•||•●•||•┼┼──🌸🌿 ɳ𝗔 ʝ𝗔ɳɘ ƙ❍ɳ 𝐬ɘ ɗ𝘂𝗔 ƙ𝘂ß❍❍ɭ ɧ𝘂i͠ ɧ𝗔ɱ𝗔r᩶i͠ ʝ❍ 𝘂𝐬ɳɘ ɱ𝘂ʝɧɘ 𝗧𝘂ʝɧ𝐬ɘ ɱi͠ɭ𝗔ɣ𝗔 ɧ❍𝗴𝗔...💜 ♥️✨ 𝗞𝗥𝗜𝗦𝗛𝗡𝗔 •||•🌸•||• ]]
+🌹✦━━━━━🌹━━🌹━━━━━✦🌹
+\n•||•_🎋𝗔𝗔p᩶ ɗ❍ɳ❍ ƙ𝗔 p᩶ɣ𝗔r᩶ 👉 [ ${tle} ] ɧ𝗔i͠ 🤐👈\n`+namee+" "+"😘"+" "+name, mentions: arraytag, attachment: imglove}
+        return api.sendMessage(msg, event.threadID, event.messageID)
+}
