c7bd405619dae5a30bbfe8b1590906b0125d2cc4
KRISHNA-LEGEND-BOY <rohitprajapati052005@gmail.com>
2025-03-12 00:37:45 +0530

    Update bf.js

--- Priyansh/commands/bf.js
+++ Priyansh/commands/bf.js
@@ -69,6 +69,6 @@ module.exports.run = async function ({ event, api, args }) {
     if (!mention[0]) return api.sendMessage("Please mention 1 person.", threadID, messageID);
     else {
         const one = senderID, two = mention[0];
-        return makeImage({ one, two }).then(path => api.sendMessage({ body: "╔═══❖••° °••❖═══╗\n\n   𝐒𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮𝐥 𝐏𝐚𝐢𝐫𝐢𝐧𝐠\n\n╚═══❖••° °••❖═══╝\n\n   ✶⊶⊷⊷❍⊶⊷⊷✶\n\n       👑 𝐌𝐢𝐥𝐥 𝐆𝐚𝐲𝐚 ❤\n\n𝐓𝐞𝐫𝐚 𝐁𝐨𝐲𝐟𝐫𝐢𝐞𝐧𝐝 🩷\n\n   ✶⊶⊷⊷❍⊶⊷⊷✶", attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
+        return makeImage({ one, two }).then(path => api.sendMessage({ body: "╔═══❖••° °••❖═══╗\n   𝐒𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮𝐥 𝐏𝐚𝐢𝐫𝐢𝐧𝐠\n╚═══❖••° °••❖═══╝\n✧═════════•❁❀❁•═════════✧\n👑 𝐌𝐢𝐥𝐥 𝐆𝐚𝐲𝐚 ❤\n𝐓𝐞𝐫𝐚 𝐁𝐨𝐲𝐟𝐫𝐢𝐞𝐧𝐝 🩷\n✧═════════•❁❀❁•═════════✧\n🌸𝐎𝐖𝐍𝐄𝐑 :-𝐌𝐑. 𝐊𝐑𝐈𝐒𝐇𝐍𝐀", attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
     }
                                               }
