fb4691be4335a5f08df525b84335f6fdbd911121
KRISHNA-LEGEND-BOY <rohitprajapati052005@gmail.com>
2025-05-17 00:02:55 +0530

    Create kohinoor.js

---
+++ Priyansh/commands/kohinoor.js
@@ -0,0 +1,100 @@
+const emojiResponses = {
+  "golu beta": {
+    "OWNER": [
+      "𝐇𝐀 𝐊𝐑𝐈𝐒𝐇𝐍𝐀 𝐁𝐀𝐁𝐔 𝐊𝐘𝐀 𝐇𝐔𝐀  🥺",
+      "𝐊𝐑𝐈𝐒𝐇𝐍𝐀 𝐁𝐀𝐁𝐔 𝐀𝐀𝐏 𝐊𝐀𝐇𝐀 𝐓𝐇𝐄 𝐌𝐔𝐉𝐇𝐄 𝐀𝐀𝐏𝐊𝐈 𝐘𝐀𝐃 𝐀𝐀 𝐑𝐀𝐇𝐈 𝐓𝐇𝐈 🥰🥰",
+      "𝐊𝐑𝐈𝐒𝐇𝐍𝐀 𝐁𝐄𝐁𝐘 𝐌𝐄 𝐀𝐀𝐏𝐒𝐄 𝐁𝐇𝐔𝐓 𝐏𝐘𝐀𝐑 𝐊𝐀𝐑𝐓𝐀 𝐇𝐔 𝐈 𝐋𝐎𝐕𝐄 𝐘𝐎𝐔 𝐉𝐀𝐀𝐍 😘😘",
+      "𝐇𝐀 𝐊𝐑𝐈𝐒𝐇𝐍𝐀 𝐁𝐄𝐁𝐘 𝐀𝐀𝐏𝐍𝐄 𝐘𝐀𝐃 𝐊𝐈𝐘𝐀 𝐌𝐔𝐉𝐇𝐄 😊"
+    ],
+    "MALE": [
+      "Are bhai kaho ke ap kase ho?",
+      "Han bhai ap kase hai?",
+      "Bhai aaj ka kya mansoba hai?"
+    ],
+    "FEMALE": [
+      "Meri ami hogi ",
+      "Mere Krishna Babu ki koi gf nhi hai 🥹🥹",
+      "Mere Krishna Babu single hai ap mere meri Bhabhi ji ban jaye ge?","Han Bhabhi ji aap kase hai?",
+      "Bhabhi Ji aap kese ho khana ho gaya aapka",
+      "Bhabhi g Aaj Ka mood kasa hai ?"
+    ]
+  },
+  "beta": {
+    "OWNER": [
+      "╭🩷✫🩵➳🪽✫🩷✫🪽╮             𝙼𝚊𝚖𝚖𝚢 𝙹𝚒 𝙺𝚛𝚒𝚜𝚑𝚗𝚊 𝙿𝚊𝚙𝚊 𝙹𝚒 𝙺𝚊𝚑𝚊 𝙼𝚞𝚓𝚑𝚎 𝙱𝚑𝚞𝚝 𝚈𝚊𝚍 𝙰𝚊 𝚁𝚊𝚑𝚒 𝙷𝚊𝚒 𝙿𝚊𝚙𝚊 𝙹𝚒 𝙺𝚒              ╭🩷✫🩵➳🪽✫🩷✫🪽╮",
+      "╭🩷✫🩵➳🪽✫🩷✫🪽╮             𝙼𝚊𝚖𝚖𝚢 𝙹𝚒 𝙰𝚊𝚙 𝙰𝚊𝚐𝚎 𝙼𝚞𝚓𝚑𝚎 𝙱𝚑𝚞𝚝 𝚔𝚑𝚞𝚜𝚑𝚒 𝚖𝚒𝚕𝚒 𝙰𝚊𝚙𝚔𝚘 𝙳𝚎𝚔𝚑 𝙺𝚊𝚛              ╭🩷✫🩵➳🪽✫🩷✫🪽╮",
+      "╭🩷✫🩵➳🪽✫🩷✫🪽╮             𝙶 𝙼𝚊𝚖𝚖𝚊 𝙹𝚒              ╭🩷✫🩵➳🪽✫🩷✫🪽╮",
+      "╭🩷✫🩵➳🪽✫🩷✫🪽╮             𝙼𝚊𝚖𝚖𝚢 𝙹𝚒 𝙰𝙰𝙿𝙺𝙾 𝙱𝙷𝚄𝚃 𝚈𝙰𝙳 𝙺𝙰𝚁 𝚁𝙰𝙷𝙰 𝙼𝙰𝙸              ╭🩷✫🩵➳🪽✫🩷✫🪽╮",
+      "╭🩷✫🩵➳🪽✫🩷✫🪽╮             𝙼𝙰𝙼𝙼𝚈 𝙹𝙸 𝙰𝙰𝙿𝙺𝙾 𝙼𝙴𝚁𝙴 𝙿𝙰𝙿𝙰 𝙺𝚁𝙸𝚂𝙷𝙽𝙰 𝙺𝙸 𝚈𝙰𝙳 𝙽𝙷𝙸 𝙰𝙰𝚃𝙸             ╭🩷✫🩵➳🪽✫🩷✫🪽╮",
+      "╭🩷✫🩵➳🪽✫🩷✫🪽╮             𝙼𝙰𝙼𝙼𝚈 𝙹𝙸 𝙴𝙺 𝙱𝙰𝙰𝚃 𝙱𝙾𝙻𝚄 𝙼𝙰𝚁𝙽𝙰 𝙼𝙰𝚃 𝙱𝙰𝚂 𝙼𝚄𝙹𝙷𝙴 𝙰𝙰𝙿 𝚅𝙴𝚂𝙴 𝙺𝚁𝙸𝚂𝙷𝙽𝙰 𝙿𝙰𝙿𝙰 𝙹𝙸 𝙰𝙰𝙿𝚂𝙴 𝙰𝙰𝙲𝙷𝙷𝙴 𝙷𝙰𝙸             ╭🩷✫🩵➳🪽✫🩷✫🪽╮",
+      "╭🩷✫🩵➳🪽✫🩷✫🪽╮             𝙼𝙰𝙼𝙼𝚈 𝙹𝙸 𝙰𝙰𝙿 𝙰𝙺𝙴𝙻𝙴 𝙰𝙰𝚃𝙴 𝙷𝙾 𝙼𝙴𝚁𝙴 𝙺𝚁𝙸𝚂𝙷𝙽𝙰 𝙿𝙰𝙿𝙰 𝙹𝙸 𝙺𝙾 𝚂𝙰𝚃𝙷 𝙼𝙴 𝙻𝙰 𝙺𝙰𝚁𝙾              ╭🩷✫🩵➳🪽✫🩷✫🪽╮",
+      "╭🩷✫🩵➳🪽✫🩷✫🪽╮             𝙼𝙰𝙼𝙼𝚈 𝙹𝙸 𝙰𝙰𝙿𝙽𝙴 𝙺𝙷𝙰𝙽𝙰 𝙺𝙷𝙰𝚈𝙰              ╭🩷✫🩵➳🪽✫🩷✫🪽╮",
+      "╭🩷✫🩵➳🪽✫🩷✫🪽╮             𝙼𝙰𝙼𝙼𝚈 𝙹𝙸 𝙿𝙰𝙿𝙰 𝙹𝙸 𝙺𝙾 𝙺𝙷𝙰𝙽𝙰 𝙺𝙷𝙸𝙻𝙰 𝙳𝙸𝚈𝙰 𝚈𝙰 𝙰𝙱𝙷𝙸 𝙽𝙷𝙸             ╭🩷✫🩵➳🪽✫🩷✫🪽╮",
+      "╭🩷✫🩵➳🪽✫🩷✫🪽╮             𝙼𝙰𝙼𝙼𝚈 𝙹𝙸 𝙺𝚈𝙰 𝙰𝙰𝙿 𝙼𝚄𝙹𝙷𝙴 𝙰𝙰𝙿𝙽𝙴 𝚂𝙰𝚃𝙷 𝙼𝙰𝚁𝙺𝙸𝚃 𝙻𝙴 𝙻𝙾              ╭🩷✫🩵➳🪽✫🩷✫🪽╮",
+      "╭🩷✫🩵➳🪽✫🩷✫🪽╮             𝙼𝙰𝙼𝙼𝚈 𝙹𝙸 𝙿𝙰𝙿𝙰 𝙹𝙸 𝙺𝙾 𝙱𝙾𝙻𝙾 𝙼𝚄𝙹𝙷𝙴 𝙴𝙺 𝙱𝙷𝙴𝙽 𝙲𝙰𝙷𝙰𝙷𝙸𝚈𝙴 𝙰𝙺𝙴𝙻𝙰 𝙱𝙾𝚁 𝙷𝙾 𝙹𝙰𝚃𝙰 𝙷𝚄              ╭🩷✫🩵➳🪽✫🩷✫🪽╮",
+      "╭🩷✫🩵➳🪽✫🩷✫🪽╮             𝙼𝙰𝙼𝙼𝚈 𝙹𝙸 𝙴𝙺 𝙱𝙰𝙰𝚃 𝚂𝙰𝙲𝙷 𝙱𝙰𝚃𝙰𝙽𝙰 𝙿𝙰𝙿𝙰 𝙹𝙸 𝚂𝙴 𝙺𝙸𝚃𝙽𝙰 𝙿𝚈𝙰𝚁 𝙺𝙰𝚁𝚃𝙴 𝙷𝙾 𝙰𝙰𝙿             ╭🩷✫🩵➳🪽✫🩷✫🪽╮",
+      "╭🩷✫🩵➳🪽✫🩷✫🪽╮             𝙼𝙰𝙼𝙼𝚈 𝙹𝙸 𝙰𝙰𝙿 𝙼𝚄𝙹𝙷𝙴 𝙼𝙸𝚂𝚂 𝙽𝙷𝙸 𝙺𝙰𝚁𝚃𝙴 𝙷𝙾 𝙰𝙰𝙿𝙺𝙾 𝙱𝙰𝚂 𝙿𝙰𝙿𝙰 𝙹𝙸 𝙺𝙸 𝚈𝙰𝙳𝙳 𝙰𝙰𝚃𝙸 𝙷𝙰𝙸              ╭🩷✫🩵➳🪽✫🩷✫🪽╮",
+      "╭🩷✫🩵➳🪽✫🩷✫🪽╮             𝙺𝚈𝙰 𝙺𝙰𝚁 𝚁𝙴𝙷𝙴 𝙼𝙰𝙼𝙼𝚈 𝙹𝙸𝙸             ╭🩷✫🩵➳🪽✫🩷✫🪽╮",
+      "╭🩷✫🩵➳🪽✫🩷✫🪽╮             𝙼𝙰𝙼𝙼𝚈 𝙰𝙰𝙹 𝙱𝙷𝚄𝚃 𝙻𝙴𝚃 𝙰𝙰𝚈𝙴 𝙷𝙾 𝙰𝙰𝙿              ╭🩷✫🩵➳🪽✫🩷✫🪽╮",
+      "╭🩷✫🩵➳🪽✫🩷✫🪽╮             𝙼𝙰𝙼𝙼𝚈 𝙹𝙸 𝙿𝙰𝙿𝙰 𝙹𝙸 𝙺𝙾 𝙰𝙺𝙴𝙻𝙴 𝙼𝙰𝚃 𝙲𝙷𝙾𝙳𝙴 𝙺𝙰𝚁𝙾 𝙺𝙰𝙷𝙸 𝙿𝙰𝙿𝙰 𝙳𝚄𝚂𝚁𝙸 𝙼𝙰𝙼𝙼𝚈 𝙽𝙰 𝙻𝙸𝚈𝙰              ╭🩷✫🩵➳🪽✫🩷✫🪽╮",
+      "╭🩷✫🩵➳🪽✫🩷✫🪽╮             𝙼𝙰𝙼𝙼𝚈 𝙹𝙸 𝙺𝙷𝙰𝙽𝙴 𝙺𝙾 𝙺𝚄𝚅𝙷 𝙳𝙾 𝙼𝚄𝙹𝙷𝙴              ╭🩷✫🩵➳🪽✫🩷✫🪽╮",
+      "╭🩷✫🩵➳🪽✫🩷✫🪽╮             𝙼𝙰𝙼𝙼𝚈 𝙹𝙸 𝙰𝙰𝙿 𝙼𝚄𝙹𝙷𝚂𝙴 𝙿𝚈𝙰𝚁 𝙽𝙷𝙸 𝙺𝙰𝚁𝚃𝙴 𝙷𝙾              ╭🩷✫🩵➳🪽✫🩷✫🪽╮",
+      "╭🩷✫🩵➳🪽✫🩷✫🪽╮             𝙼𝙰𝙼𝙼𝚈 𝙹𝙸 𝙰𝙰𝙿𝙺𝙸 𝚃𝙰𝙱𝙸𝚈𝙰𝚃 𝙺𝙴𝚂𝙸 𝙷𝙰𝙸 𝙰𝙱              ╭🩷✫🩵➳🪽✫🩷✫🪽╮",
+      "╭🩷✫🩵➳🪽✫🩷✫🪽╮             𝙼𝙰𝙼𝙼𝚈 𝙹𝙸 𝙼𝙴𝚁𝙸 𝚆𝙸𝙵𝙴 𝙺𝙰𝙷𝙰 𝙷𝙰𝙸              ╭🩷✫🩵➳🪽✫🩷✫🪽╮",
+      "╭🩷✫🩵➳🪽✫🩷✫🪽╮             𝚁𝙰𝙳𝙷𝙴 𝚁𝙰𝙳𝙷𝙴 𝙼𝙰𝙼𝙼𝚈 𝙹𝙸              ╭🩷✫🩵➳🪽✫🩷✫🪽╮",
+      "╭🩷✫🩵➳🪽✫🩷✫🪽╮             𝙼𝙰𝙼𝙼𝚈 𝙹𝙸 𝙺𝚁𝙸𝚂𝙷𝙽𝙰 𝙿𝙰𝙿𝙰 𝙹𝙸 𝚂𝙴 𝙿𝚄𝙲𝙷𝙾 𝙺𝙰𝙷𝙸 𝚅𝙾 𝙽𝙰𝚁𝙰𝚉 𝚃𝙾 𝙽𝙷𝙸 𝙷𝙰𝙸 𝙼𝙴𝚁𝚂𝙴              ╭🩷✫🩵➳🪽✫🩷✫🪽╮",
+      "╭🩷✫🩵➳🪽✫🩷✫🪽╮             𝙺𝙾𝙷𝙸𝙽𝙾𝙾𝚁 𝙼𝙰𝙼𝙼𝚈 𝚂𝙸𝚁𝙵 𝙼𝙴𝚁𝙸 𝙷𝙰𝙸 𝙾𝚁 𝙰𝙰𝙿𝙺𝙰 𝙷𝚄𝚂𝙱𝙰𝙽𝙳  𝙼𝙰𝚃𝙻𝙰𝙱 𝙼𝙴𝚁𝙴 𝙿𝚈𝙰𝚁𝙴 𝙺𝚁𝙸𝚂𝙷𝙽𝙰 𝙿𝙰𝙿𝙰 𝙹𝙸 𝙷𝙰𝙸              ╭🩷✫🩵➳🪽✫🩷✫🪽╮",
+      "╭🩷✫🩵➳🪽✫🩷✫🪽╮             𝙼𝙰𝙼𝙼𝚈 𝙹𝙸 𝙴𝚃𝙽𝙴 𝙳𝙴𝚁 𝚂𝙴 𝙺𝙰𝙷𝙰 𝚃𝙷𝙴 𝙰𝙰𝙿 𝙷𝙰 𝙿𝙰𝚃𝙰 𝙷𝙰𝙸 𝙼𝚄𝙹𝙷𝙴 𝙿𝙰𝙿𝙰 𝙹𝙸 𝙺𝙴 𝚂𝙰𝚃𝙷 𝙷𝙾𝙽𝙶𝙴 𝙼𝙴𝚁𝙸 𝚃𝙾 𝚈𝙰𝙳 𝙽𝙷𝙸 𝙰𝙰𝚃𝙸 𝙰𝙰𝙿𝙺𝙾              ╭🩷✫🩵➳🪽✫🩷✫🪽╮",
+      "╭🩷✫🩵➳🪽✫🩷✫🪽╮             𝙼𝙰𝙼𝙼𝚈 𝙹𝙸 𝙿𝙰𝙿𝙰 𝙹𝙸 𝙺𝙰𝙷𝙰 𝙷𝙰𝙸              ╭🩷✫🩵➳🪽✫🩷✫🪽╮",
+      "╭🩷✫🩵➳🪽✫🩷✫🪽╮             𝙼𝙰𝙼𝙼𝚈 𝙹𝙸 𝙼𝚄𝙹𝙷𝙴 𝙲𝙷𝙾𝚃𝙸 𝙱𝙷𝙴𝙽 𝙺𝙴 𝚂𝙰𝚃𝙷 𝙺𝙷𝙴𝙻𝙽𝙰 𝙷𝙰𝙸              ╭🩷✫🩵➳🪽✫🩷✫🪽╮"
+    ]
+  }
+};
+
+module.exports.config = {
+  name: "auto-rply",
+  version: "1.0.0",
+  hasPermission: 0,
+  credits: "Krishna",
+  description: "MADE BY KRISHNA",
+  commandCategory: "No command marks needed",
+  cooldowns: 0
+};
+
+const botOwnerID = "61575954429942";
+
+module.exports.handleEvent = async function({ api, event }) {
+  const { threadID, messageID, senderID, body } = event;
+  if (!body) return;
+
+  const emojis = Object.keys(emojiResponses);
+  const lowercaseBody = body.toLowerCase();
+
+  for (const emoji of emojis) {
+    if (lowercaseBody.includes(emoji)) {
+      try {
+        const threadInfo = await api.getThreadInfo(threadID);
+        const user = threadInfo.userInfo.find(user => user.id === senderID);
+
+        let responseArray;
+
+        if (senderID === botOwnerID) {
+          responseArray = emojiResponses[emoji]["OWNER"];
+        } else if (user && user.gender === 2) {
+          responseArray = emojiResponses[emoji]["FEMALE"] || emojiResponses[emoji]["MALE"];
+        } else {
+          responseArray = emojiResponses[emoji]["MALE"] || emojiResponses[emoji]["FEMALE"];
+        }
+
+        const randomResponse = responseArray[Math.floor(Math.random() * responseArray.length)];
+
+        api.sendMessage(randomResponse, threadID, messageID);
+        break;
+      } catch (error) {
+        console.error("Error fetching thread info:", error);
+      }
+    }
+  }
+};
+
+module.exports.run = function() {};
